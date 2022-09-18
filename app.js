71957c9b557ae2ea2794c44cc5c347120c849e4b
its-kfelix <felixodhiambo538@gmail.com>
2022-09-18 16:10:10 +0300

    Added active tabs

--- app.js
+++ app.js
@@ -4,4 +4,75 @@ const menuLinks = document.querySelector('.navbar__menu');
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
-})
+});
+// const activePage= window.location.pathname;
+// const navLinks= document.querySelectorAll('nav a').
+// forEach(link =>{
+//     if(link.href.includes('${activePage}')){
+//         // link.classList.add('active');
+//         // console.log(link);
+//         console.log(activePage)
+//     }
+// })
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+});
+
+// scroll animation effect
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+// var btnContainer = document.getElementById("tabs");
+
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("navbar__item");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
